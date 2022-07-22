35c0990bf7a835e633db96fe04c736d1085675cc
Thuotracy <tracyjacobs775@gmail.com>
2022-07-22 22:00:15 +0300

    made the timer funtional

--- main.js
+++ main.js
@@ -10,6 +10,20 @@ var s = document.getElementById('sec');
 var startTimer = null;
 
 function timer(){
+    if (h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if (s.value != 0){
+        s.value--;
+    } else if (m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        m.value = 60;
+        h.value--;
+    } 
+    return;
 
 }
 
