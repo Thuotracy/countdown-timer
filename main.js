46e357cf2d17c8ca72f0c69aa344b80b97b55fe2
Thuotracy <tracyjacobs775@gmail.com>
2022-07-22 22:12:12 +0300

    modified the styling of the button

--- main.js
+++ main.js
@@ -1,48 +1,50 @@
 var start = document.getElementById('start');
 var reset = document.getElementById('reset');
 
-var h = document.getElementById('hour');
-var m = document.getElementById('minute');
-var s = document.getElementById('sec');
+var h = document.getElementById("hour");
+var m = document.getElementById("minute");
+var s = document.getElementById("sec");
 
-
-// store a  reference to the variable
+//store a reference to the startTimer variable
 var startTimer = null;
 
-function timer(){
-    if (h.value == 0 && m.value == 0 && s.value == 0){
-        h.value = 0;
-        m.value = 0;
-        s.value = 0;
-    } else if (s.value != 0){
-        s.value--;
-    } else if (m.value != 0 && s.value == 0){
-        s.value = 59;
-        m.value--;
-    } else if(h.value != 0 && m.value == 0){
-        m.value = 60;
-        h.value--;
-    } 
-    return;
-
-}
-
-function stopTimer(){
-    clearInterval(startTimer);
-}
-
 start.addEventListener('click', function(){
-    // initialise the variable startTimer
+    //initialize the variable
     function startInterval(){
         startTimer = setInterval(function() {
             timer();
         }, 1000);
     }
+    startInterval();
 })
 
 reset.addEventListener('click', function(){
     h.value = 0;
     m.value = 0;
     s.value = 0;
-    stopTimer()
+    //stop the timer after pressing "reset"
+    stopInterval()
 })
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        m.value = 60;
+        h.value--;
+    }
+    return;
+}
+
+//stop the function after pressing the reset button, 
+//so the time wont go down when selecting a new time after pressing reset
+function stopInterval() {
+    clearInterval(startTimer);
+}
